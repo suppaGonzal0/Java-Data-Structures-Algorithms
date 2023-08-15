5a8705c73d92ea0fd16fe86d0ed774e0b2bd109b
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-08-15 12:38:21 +0600

    Update fibonacci.java

--- Dynamic Programming/fibonacci.java
+++ Dynamic Programming/fibonacci.java
@@ -7,3 +7,20 @@ Dynamic Programming solution:
   - Time Complexity: n
   - Space Complexity: n
 */
+
+class Fibonacci {
+
+    public static Map<Integer, Integer> map = new HashMap<>();
+
+    public int fib(int n) {
+        if(n <= 0) return 0;
+
+        if(n <= 2) return 1;
+
+        if(map.containsKey(n)) return map.get(n);
+
+        map.put(n, fib(n-1)+fib(n-2));
+
+        return map.get(n);
+    }
+}
